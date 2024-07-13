c18dce99b1f61686d2be1392f015434d2d7fac96
igwe uchenna <fesimaxu30@gmail.com>
2024-07-13 11:37:36 +0100

    update fix

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import dotenv from "dotenv";
@@ -12,9 +13,11 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(UserPreferenceRoute);
 
+app.options('*', cors());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || '', {
   } as ConnectOptions) 
